1a31dcb474871db1d55cfd6ebe59133709ae2ebe
EC2 Default User <kairyu.bonvivant@gmail.com>
2021-12-24 05:22:31 +0000

    Add devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :stores
-  devise_for :endusers
-  devise_for :admins
+  devise_for :stores,skip: [:passwords,], controllers: {
+  registrations: "store/registrations",
+  sessions: 'store/sessions'
+}
+  devise_for :endusers,skip: [:passwords,], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
