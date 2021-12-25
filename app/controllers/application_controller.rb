e886d6f663928afb4ca0058bbdd1d1aa4ac23889
EC2 Default User <kairyu.bonvivant@gmail.com>
2021-12-25 04:13:58 +0000

    [Add 各ログイン先、ログアウト先]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,24 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:name_kana,:post_address,:address,:phone_number])
   end
+
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+      admin_root_path
+    elsif resource_or_scope.is_a?(Store)
+      store_path(current_store.id)
+    else
+      enduser_path(current_enduser.id)
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path
+    elsif resource_or_scope == :store
+      new_store_session_path
+    else
+      root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :stores,skip: [:passwords,], controllers: {
-  registrations: "store/registrations",
-  sessions: 'store/sessions'
-}
   devise_for :endusers,skip: [:passwords,], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
+}
+  devise_for :stores,skip: [:passwords,], controllers: {
+  registrations: "store/registrations",
+  sessions: 'store/sessions'
 }
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
@@ -17,16 +17,7 @@ namespace :admin do
   resources :stores,only:[:index,:show,:edit]
 end
 
-scope module: :store do
-  root to: 'homes#top'
-  resources :items,except:[:destroy]
-  resources :genres,except:[:show,:destroy,:new]
-  resources :endusers,except:[:new,:create,:destroy]
-  resources :orders,only:[:show,:update]
-  resources :order_items,only:[:update]
-end
-
- scope module: :public do
+scope module: :public do
     root to: 'homes#top'
     get 'about' => 'homes#about'
     resources :items,only:[:index,:show]
@@ -36,10 +27,20 @@ end
     get 'orders/complete' => 'orders#complete'
     resources :orders,except:[:edit]
     resources :endusers,except:[:destroy] do
-      get 'unsubscribe' => 'endusers#unsubscribe'
-      patch 'withdraw' => 'endusers#withdraw'
       resources :deliveries,except:[:show]
     end
   end
+
+scope module: :store do
+  root to: 'homes#top'
+  resources :stores,only:[:index,:show,:edit]
+  get 'unsubscribe' => 'stores#unsubscribe'
+  patch 'withdraw' => 'stores#withdraw'
+  resources :items,except:[:destroy]
+  resources :genres,except:[:show,:destroy,:new]
+  resources :endusers,except:[:new,:create,:destroy]
+  resources :orders,only:[:show,:update]
+  resources :order_items,only:[:update]
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
