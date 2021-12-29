d1501e1cc0a1cacb00932385c5acf3436d4c75a3
EC2 Default User <kairyu.bonvivant@gmail.com>
2021-12-29 09:34:16 +0000

    [Add enduser商品一覧]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,8 +2,10 @@ class Public::ItemsController < ApplicationController
   before_action :authenticate_enduser!
 
   def index
+    @items = Item.page(params[:page])
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 end
