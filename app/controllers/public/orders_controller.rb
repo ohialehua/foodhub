e871d190d1ba124146645e7f5e17d9302d53e6bb
EC2 Default User <kairyu.bonvivant@gmail.com>
2021-12-30 11:03:56 +0000

    [Fix Order.Detail カラム]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -30,7 +30,7 @@ class Public::OrdersController < ApplicationController
     order.enduser_id = current_enduser.id
     if order.save
       current_enduser.cart_items.each do |c|
-        OrderDetail.create(order_id: order.id, store_id: c.store_id, item_id: c.item_id, order_quantity: c.quantity, price_after_tax: c.item.with_tax_price)
+        OrderDetail.create(order_id: order.id, item_id: c.item_id, order_quantity: c.quantity, price_after_tax: c.item.with_tax_price)
       end
       current_enduser.cart_items.destroy_all
       redirect_to orders_complete_path
