bd84d401c27a24c5eddea47c173c1c15ce792b5f
ohialehua <kairyu.bonvivant@gmail.com>
2022-01-10 09:41:50 +0000

    [enduserフォロー・フォロワー一覧]

--- app/controllers/public/endusers_controller.rb
+++ app/controllers/public/endusers_controller.rb
@@ -2,7 +2,9 @@ class Public::EndusersController < ApplicationController
   before_action :authenticate_enduser!
 
   def index
-    @endusers = Enduser.page(params[:page])
+    @endusers = Enduser.page(params[:enduser_page]).per(1)
+    @followings = current_enduser.followings.page(params[:following_page]).per(1)
+    @followers = current_enduser.followers.page(params[:follower_page]).per(1)
   end
 
   def show
