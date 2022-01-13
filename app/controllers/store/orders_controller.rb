6fd9a7aa70ee868e5ce0f798b345fc8197f7782d
ohialehua <kairyu.bonvivant@gmail.com>
2022-01-13 05:19:43 +0000

    [Add store注文履歴詳細]

--- app/controllers/store/orders_controller.rb
+++ app/controllers/store/orders_controller.rb
@@ -2,9 +2,12 @@ class Store::OrdersController < ApplicationController
   before_action :authenticate_store!
 
   def index
+    @orders=current_store.orders.page(params[:page])
   end
 
   def show
+    @order = Order.find(params[:id])
+    @postage = 200
   end
 
 end
