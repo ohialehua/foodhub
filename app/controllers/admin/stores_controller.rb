b6b2132b102970e4133a7866efa7c927b09db9f5
ohialehua <kairyu.bonvivant@gmail.com>
2022-01-19 10:09:32 +0000

    [Add admin管理者機能]

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -2,10 +2,21 @@ class Admin::StoresController < ApplicationController
   before_action :authenticate_admin!
 
   def show
-    @store = Store.find(params[:store_id])
+    @store = Store.find(params[:id])
+    @items = @store.items.page(params[:item_page])
+    @posts = @store.posts.page(params[:post_page])
+    @genres = @store.genres
   end
 
-  def edit
-    @store = Store.find(params[:store_id])
+  def update
+    store = Store.find(params[:id])
+    store.update(store_params)
+    redirect_to request.referer
+  end
+
+   private
+
+  def store_params
+    params.require(:store).permit(:is_deleted)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
 namespace :admin do
   root to: 'homes#top'
-  resources :stores,only:[:show,:edit]
+  resources :stores,only:[:show,:update]
 end
 
 namespace :store do
