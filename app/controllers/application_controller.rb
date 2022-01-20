f5737a01695bb27a5c69698696e7b569ad352876
ohialehua <kairyu.bonvivant@gmail.com>
2022-01-20 05:14:13 +0000

    [Add storeサインアップ後ログアウト機能]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,4 +26,5 @@ class ApplicationController < ActionController::Base
       root_path
     end
   end
+
 end
--- app/controllers/store/homes_controller.rb
+++ app/controllers/store/homes_controller.rb
@@ -1,9 +1,19 @@
 class Store::HomesController < ApplicationController
   before_action :authenticate_store!
+  before_action :ensure_store_status
 
   def top
     @store = current_store
     @items = @store.items.page(params[:item_page])
     @posts = @store.posts.page(params[:post_page]).reverse_order
   end
+
+  private
+
+  def ensure_store_status
+    if current_store.is_deleted == true
+      sign_out_and_redirect(current_store)
+    end
+  end
+
 end
