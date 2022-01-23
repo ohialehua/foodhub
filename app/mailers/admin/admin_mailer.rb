4762cc6c4a8fe119cc6d70954f8ebf65027841dd
ohialehua <kairyu.bonvivant@gmail.com>
2022-01-23 05:24:10 +0000

    [Fix Mailer]

--- app/mailers/admin/admin_mailer.rb
+++ app/mailers/admin/admin_mailer.rb
@@ -1,12 +1,12 @@
 class Admin::AdminMailer < ApplicationMailer
 
-  def welcome_email
+  def welcome_email(store)
     @store = params[:store]
     @url = 'https://df280bcad38f46449527229623af10d2.vfs.cloud9.ap-northeast-1.amazonaws.com/stores/sign_in'
     mail(to: @store.email, subject: 'Foodhubへようこそ')
   end
 
-  def warning_email
+  def warning_email(store)
     @store = params[:store]
     mail(to: @store.email, subject: 'Foodhubのアカウントが無効化されました')
   end
