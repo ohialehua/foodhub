2fd2ca59dc2e64e4583c401166ad417d12b17b95
ohialehua <kairyu.bonvivant@gmail.com>
2022-01-27 09:21:11 +0000

    [Fix public商品ステータス]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -13,5 +13,8 @@ class Public::ItemsController < ApplicationController
     @cart_items = current_enduser.cart_items
     @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
     @store = @item.store
+    unless @item.is_active == true
+      flash[:danger] = "この商品は現在販売停止中のため購入できません"
+    end
   end
 end
