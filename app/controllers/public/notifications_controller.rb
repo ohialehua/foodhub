30725ea0f2fd9c3439e3ff67820211849f7ff542
ohialehua <kairyu.bonvivant@gmail.com>
2022-02-07 12:20:52 +0000

    [Add icon]

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,2 +1,11 @@
 class Public::NotificationsController < ApplicationController
+  before_action :authenticate_enduser!
+  
+  def index
+    @notifications = current_enduser.passive_public_notifications.page(params[:page]).per(20)
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
+  end
+  
 end
