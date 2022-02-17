cf9d497218bf766ee3aaceb73a64dadc6b739bd2
ohialehua <kairyu.bonvivant@gmail.com>
2022-02-17 12:30:08 +0000

    [Add コメントアウト]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -5,6 +5,8 @@ class Public::ItemsController < ApplicationController
     @items = Item.sort(params[:selection]).page(params[:page])
     @rank_items = Item.find(OrderDetail.rank(5).pluck(:item_id))
     #注文詳細にあるランキング上位5個までの商品IDを取得
+    #この取得の仕方だとOrderDetailに紐づいているitemであるため、itemを削除した後に削除したitemがランキングに入っているとIDが取得できないエラーになる。
+    #思いつく解消法はItemのカラムに"今までに購入された個数"を付け加え、購入されるたびにその個数が更新されるようにし、そのカラムからランキングに並び変える方法。
     @markers = current_enduser.markers
   end
 
